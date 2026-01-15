75eba722a665274078c8bd0def09342815dd2b42
Allan-QLB <qin_libin@foxmail.com>
2026-01-15 20:58:25 +0800

    avoid duplicate admin jmx-name

--- sps4j-examples/spring-boot2-example/host-application/src/main/java/com/github/sps4j/example/springmvc/host/Main.java
+++ sps4j-examples/spring-boot2-example/host-application/src/main/java/com/github/sps4j/example/springmvc/host/Main.java
@@ -5,10 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
-    static {
-        System.setProperty("spring.application.admin.enabled", "false");
-    }
-
     public static void main(String[] args) {
        SpringApplication.run(Main.class, args);
     }
--- sps4j-spring-boot2/src/main/java/com/github/sps4j/springboot2/misc/Sps4jRunListener.java
+++ sps4j-spring-boot2/src/main/java/com/github/sps4j/springboot2/misc/Sps4jRunListener.java
@@ -5,8 +5,11 @@ import com.github.sps4j.springboot2.context.HostApplicationContextHolder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringApplicationRunListener;
-import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.MapPropertySource;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A {@link SpringApplicationRunListener} for SPS4J.
@@ -31,10 +34,15 @@ public class Sps4jRunListener implements SpringApplicationRunListener {
     public void contextPrepared(ConfigurableApplicationContext context) {
         boolean baseApp = !(Thread.currentThread().getContextClassLoader() instanceof Sps4jPluginClassLoader);
         if (baseApp) {
-            HostApplicationContextHolder.create((ServletWebServerApplicationContext) context);
+            HostApplicationContextHolder.create(context);
             log.info("Application context prepared for base application");
         } else {
             log.info("Application context prepared for plugin application, not register shutdown hook");
+            Map<String, Object> source = new HashMap<>();
+            source.put("spring.application.admin.jmx-name",
+                    "org.springframework.boot:type=Admin,name=" + context.getApplicationName());
+            context.getEnvironment().getPropertySources()
+                    .addFirst(new MapPropertySource("sps4j-plugin-override", source));
             application.setRegisterShutdownHook(false);
         }
     }
