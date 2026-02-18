af822943f076ffa6f979f099a00a8d6c23515cc4
qchole <qin_libin@foxmail.com>
2026-02-18 08:38:00 +0800

    add configuration property to enable/disable adding servlet context path prefix

--- sps4j-spring-boot2/src/main/java/io/github/sps4j/springboot2/config/Sps4jSpringMvcAutoConfiguration.java
+++ sps4j-spring-boot2/src/main/java/io/github/sps4j/springboot2/config/Sps4jSpringMvcAutoConfiguration.java
@@ -6,9 +6,11 @@ import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.autoconfigure.web.servlet.ServletWebServerFactoryAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,10 +27,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnExpression("T(java.lang.Thread).currentThread().contextClassLoader instanceof T(io.github.sps4j.core.load.Sps4jPluginClassLoader)")
 @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
-@AutoConfigureBefore({ServletWebServerFactoryAutoConfiguration.class, JettyAutoConfiguration.class})
+@AutoConfigureBefore({ServletWebServerFactoryAutoConfiguration.class, JettyAutoConfiguration.class,
+        TomcatAutoConfiguration.class})
+@EnableConfigurationProperties(Sps4jSpringMvcProperties.class)
 public class Sps4jSpringMvcAutoConfiguration {
 
     @Bean
+    @ConditionalOnProperty(prefix = "sps4j.spring-mvc", name = "add-servlet-context-prefix", havingValue = "true", matchIfMissing = true)
     public AddPluginServletContextPathCustomizer addPluginServletContextPathCustomizer(ServerProperties serverProperties) {
         return new AddPluginServletContextPathCustomizer(serverProperties);
     }
